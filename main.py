4377078ff370ecfee1da9cf47601c982e03c71d5
Jack Cook <hello@jackcook.nyc>
2016-04-03 01:42:43 -0400

    Cleaned up the code

--- main.py
+++ main.py
@@ -1,68 +1,81 @@
-from flask import Flask, request
+from bs4 import BeautifulSoup
 import flask
+from flask import Flask, request
 import json
 import requests
 from requests.auth import HTTPBasicAuth
-from bs4 import BeautifulSoup
-import urllib
 
 app = Flask(__name__)
 
-articleList = []
+article_list = []
 
-def send_request():
-    try:
-        url = "https://api.datamarket.azure.com/Bing/Search/v1/Composite"
+def retrieve_articles(retrieve_text):
+    url = "https://api.datamarket.azure.com/Bing/Search/v1/Composite"
+    token = "uV5LSCwIXoqjVyZ2Y5C4S9nHpsGzuOS6u/0eKHtHcn4"
+    response = requests.get(url,
+        auth = HTTPBasicAuth(token, token),
+        params = {
+            "Sources": "'news'",
+            "Query": "''",
+            "Market": "'fr-FR'",
+            "$format": "json",
+        },
+        headers = {
+            "Authorization": "Basic dVY1TFNDd0lYb3FqVnlaMlk1QzRTOW5IcHNHenVPUzZ1LzBlS0h0SGNuNDoqKioqKiBIaWRkZW4gY3JlZGVudGlhbHMgKioqKio=",
+        }
+    )
 
-        resp=requests.get(url, auth=HTTPBasicAuth('uV5LSCwIXoqjVyZ2Y5C4S9nHpsGzuOS6u/0eKHtHcn4', 'uV5LSCwIXoqjVyZ2Y5C4S9nHpsGzuOS6u/0eKHtHcn4'),
-            params={
-                "Sources": "'news'",
-                "Query": "''",
-                "Market": "'fr-FR'",
-                "$format": "json",
-            },
-            headers={
-                "Authorization": "Basic dVY1TFNDd0lYb3FqVnlaMlk1QzRTOW5IcHNHenVPUzZ1LzBlS0h0SGNuNDoqKioqKiBIaWRkZW4gY3JlZGVudGlhbHMgKioqKio=",
-            },
-        )
+    articles = json.loads(response.content)["d"]["results"][0]["News"]
+    for article in articles:
+        url = article["Url"]
+        article_html = requests.get(url).content
+        soup = BeautifulSoup(article_html, "html.parser")
 
-        myNews = json.loads(resp.content)["d"]["results"][0]["News"]
+        imageFind = soup.findAll(attrs={"property": "og:image"})
+        image = imageFind[0]["content"].encode("utf-8")
 
-        for news_obj in myNews:
-            url = news_obj["Url"]
-            response = requests.get(url).content
-            soup3 = BeautifulSoup(response, "html.parser")
+        titleFind = soup.findAll(attrs={"property": "og:title"})
+        title = titleFind[0]["content"].encode("utf-8")
 
-            desc = soup3.findAll(attrs={"property":"og:image"})
-            image = desc[0]['content'].encode('utf-8')
+        descriptionFind = soup.findAll(attrs={"property": "og:description"})
+        description = ""
 
-            desc2=soup3.findAll(attrs={"property":"og:title"})
-            title=desc2[0]['content'].encode('utf-8')
+        if len(descriptionFind) > 0:
+            description = descriptionFind[0]["content"].encode("utf-8")
 
-            desc3=soup3.findAll(attrs={"property":"og:description"})
-            description = ""
-            if len(desc3) > 0:
-                description=desc3[0]['content'].encode('utf-8')
-            #og:title og:description
+        article = {
+            "url": url,
+            "image": image,
+            "title": title,
+            "description": description
+        }
 
-            try:
-                response = requests.get(
-                    url="http://api.diffbot.com/v3/article",
-                    params={
-                        "token": "0d5c56d2a7a3a5a4ad6c644b326993c2",
-                        "url": "http://www.lemonde.fr/proche-orient/article/2016/04/02/un-charnier-de-l-etat-islamique-decouvert-a-palmyre-en-syrie_4894601_3218.html",
-                    },
-                )
-                content=json.loads(response.content)["objects"][0]["text"]
-                article = {"url": url, "image": image, "title":title, "description":description, "text": content}
-                articleList.append(article)
-                print article
-            except requests.exceptions.RequestException:
-                print('HTTP Request failed')
-    except requests.exceptions.RequestException:
-        print('HTTP Request failed')
+        if retrieve_text:
+            diffbotResponse = requests.get(
+                url = "http://api.diffbot.com/v3/article",
+                params = {
+                    "token": "0d5c56d2a7a3a5a4ad6c644b326993c2",
+                    "url": "http://www.lemonde.fr/proche-orient/article/2016/04/02/un-charnier-de-l-etat-islamique-decouvert-a-palmyre-en-syrie_4894601_3218.html",
+                },
+            )
 
-        #response obj 0 text
+            articleText = json.loads(diffbotResponse.content)["objects"][0]["text"]
+            article["text"] = articleText
+
+        article_list.append(article)
+
+def translate_term(term):
+    response = requests.get(
+        url = "https://www.googleapis.com/language/translate/v2",
+        params = {
+            "key": "AIzaSyAqM11ClyXJss3ETYmhNFUWFWN5PkbSuo4",
+            "q": term,
+            "source": "fr",
+            "target": "en",
+        },
+    )
+
+    print response.content
 
 @app.route("/articles")
 def articles():
@@ -73,26 +86,8 @@ def articles():
 def translate():
     term = request.args.get("term")
 
-def send_request2(term):
-    try:
-        response = requests.get(
-            url="https://www.googleapis.com/language/translate/v2",
-            params={
-                "key": "AIzaSyAqM11ClyXJss3ETYmhNFUWFWN5PkbSuo4",
-                "q": term,
-                "source": "fr",
-                "target": "en",
-            },
-        )
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
-    except requests.exceptions.RequestException:
-        print('HTTP Request failed')
-
 if __name__ == "__main__":
-    send_request()
-    #send_request3()
-    # send_request2("la lait")
+    retrieve_articles(False)
+    # translate_term("la lait")
+
     app.run(debug=True)
