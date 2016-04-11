86db955a049656be9fa4f1486025170a3b15b6ce
Jack Cook <hello@jackcook.nyc>
2016-04-10 20:16:39 -0400

    Save articles by phone number

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import flask
 from flask import Flask, g, request
 import json
 from newspaper import Article
+import os
 import requests
 import sqlite3
 
@@ -28,12 +29,17 @@ def translate_term(term, language):
 @app.before_request
 def before_request():
     g.db = connect_db()
+    g.sdb = connect_saved_db()
 
 @app.teardown_request
 def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
+
+    sdb = getattr(g, 'sdb', None)
+    if sdb is not None:
+        sdb.close()
 
 @app.route("/articles")
 def articles():
@@ -63,6 +69,32 @@ def parse():
 
     return flask.jsonify(**article)
 
+@app.route("/save", methods=['POST'])
+def save():
+    num = request.args.get("num")
+    url = request.args.get("url")
+
+    g.sdb.execute("""create table if not exists n%s (
+        id integer primary key autoincrement,
+        url text not null,
+        status integer not null
+    )""" % num)
+
+    g.sdb.execute("insert into n%s (url, status) values ('%s', 0)" % (num, url))
+    g.sdb.commit()
+
+    return flask.jsonify(**{"success": True})
+
+@app.route("/saved")
+def saved():
+    num = request.args.get("num")
+
+    cur = g.sdb.execute("select * from n%s" % num)
+    entries = [dict(url=row[1], status=row[2]) for row in cur.fetchall()]
+    result = {"entries": entries}
+
+    return flask.jsonify(**result)
+
 @app.route("/translate")
 def translate():
     term = request.args.get("term")
@@ -82,5 +114,14 @@ def translate():
 def connect_db():
     return sqlite3.connect("data.db")
 
+def connect_saved_db():
+    return sqlite3.connect("saved.db")
+
 if __name__ == "__main__":
+    if not os.path.isfile("data.db"):
+        file("data.db", "w+")
+
+    if not os.path.isfile("saved.db"):
+        file("saved.db", "w+")
+
     app.run(debug=True, host="0.0.0.0", port=80)
