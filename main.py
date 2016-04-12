60695d05cdd64fb5b12cbc6d80197cabd5054c65
claireglendening <glendenc@bxscience.edu>
2016-04-11 20:49:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import requests
 import sqlite3
 
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -45,7 +46,7 @@ def teardown_request(exception):
 def articles():
     language = request.args.get("lang")
     fetched_articles = []
-    cur = g.db.execute('select * from articles where language=\"%s\"' % language)
+    cur = g.db.execute('select * from articles where language=\"%s\"' % (language)
     entries = [(dict(url=row[1], title=row[2], text=row[3], image=row[4]) if len(row[4]) > 0 else dict(url=row[1], title=row[2], text=row[3])) for row in cur.fetchall()]
     articles = {"articles": entries}
     return flask.jsonify(**articles)
@@ -68,11 +69,17 @@ def parse():
     }
 
     return flask.jsonify(**article)
-
+    
 @app.route("/save", methods=['POST'])
 def save():
     num = request.args.get("num")
     url = request.args.get("url")
+
+    a = Article(url)
+    a.download()
+    a.parse()
+
+    text = a.text.replace("\"", "'")
 
     g.sdb.execute("""create table if not exists n%s (
         id integer primary key autoincrement,
@@ -88,10 +95,20 @@ def save():
 @app.route("/saved")
 def saved():
     num = request.args.get("num")
+    url = request.args.get("url")
+    a = Article(url)
+    a.download()
+    a.parse()
 
     cur = g.sdb.execute("select * from n%s" % num)
-    entries = [dict(url=row[1], status=row[2]) for row in cur.fetchall()]
-    result = {"entries": entries}
+    entries = [dict(url=row[1], status=row[2],title=row[3], text=row[3],image=row[5],num=row[6]) for row in cur.fetchall()]
+    result = {
+        "entries": entries;
+        "url":url
+        "image":a.top_image,
+        "title":a.title.replace("\"","''),
+        "text":text
+        }
 
     return flask.jsonify(**result)
 
